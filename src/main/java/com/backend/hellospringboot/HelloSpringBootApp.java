138b5a3afe8a080c721d4e12de3b8d29ba1fd6b8
Tulio <tuliowetler@gmail.com>
2025-04-28 22:18:40 +0100

    Updated

---
+++ src/main/java/com/backend/hellospringboot/HelloSpringBootApp.java
@@ -0,0 +1,12 @@
+package com.backend.hellospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootApp.class, args);
+    }
+}
