f6dc02209f992b66dea3dc3c15615208ec29ce64
Tulio <tuliowetler@gmail.com>
2025-04-29 01:24:04 +0100

    Returning propper JSON

--- src/main/java/com/backend/hellospringboot/HelloController.java
+++ src/main/java/com/backend/hellospringboot/HelloController.java
@@ -1,13 +1,22 @@
 package com.backend.hellospringboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
+// Controllers
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+
+import java.util.Map;
+import java.util.HashMap;
+
+// Rest API's
 @RestController
 public class HelloController {
-
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    public Map<String, String> say_hi() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hey, your test has worked");
+        return response;
     }
 }
+
+
