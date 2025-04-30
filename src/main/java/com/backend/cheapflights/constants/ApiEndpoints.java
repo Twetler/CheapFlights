5bb8414b8b76f8b7e55a53aca612a12bb8b0d57a
Tulio <tuliowetler@gmail.com>
2025-04-30 09:01:02 +0100

    API Constants

---
+++ src/main/java/com/backend/cheapflights/constants/ApiEndpoints.java
@@ -0,0 +1,9 @@
+package com.backend.cheapflights.constants;
+
+public class ApiEndpoints {
+    public static final String base_url = "https://www.ryanair.com/api";
+
+    public static class Airports {
+        public static final String LIST_ALL = base_url + "/views/locate/5/airports/en/active";
+    }
+}
